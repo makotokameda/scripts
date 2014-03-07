8fa1ae11de1fa8f5d30369a74019722245b73e2b
Makoto Kameda <makoto.kameda@gmail.com>
2014-03-07 05:24:45 +0100

    ['#111111', '#22222', '#33333']

--- commit-msg.py
+++ commit-msg.py
@@ -24,8 +24,10 @@ for piv_id in pivStory_ids:
     if m != None:
         final_pids.append('#' + piv_id)
 
-f = codecs.open(msg_file, "w", "utf-8")
+f = codecs.open(msg_file, "w+", "utf-8")
+all = f.read()
 print(final_pids, file=f)
+print(all, file=f)
 f.close()
 
 
