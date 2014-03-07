e1a28e65d377c689a16b284250b26ad07cd56d61
Makoto Kameda <makoto.kameda@gmail.com>
2014-03-07 05:12:58 +0100

    finished commit-msg ['#111111', '#22222', '#33333'] finished commit-msg

--- commit-msg.py
+++ commit-msg.py
@@ -1,17 +1,37 @@
 #!/usr/local/bin/python3
+import os
+import sys
 import subprocess
 import re
+import codecs
+
+msg_file = sys.argv[1]
+#msg_file = "README.md"
+
 
 branches = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], universal_newlines=True)
-a = re.match("-(.*?)$", branches.strip("\n"))
-pivStoryIDs = a.group(1).split(".")
+brs = re.search("-(.*?)$", branches.strip("\n"))
+if brs == None:
+    print('fertig')
+pivStory_ids = brs.group(1).split(",")
+
 pivURL = "https://www.pivotaltracker.com/story/show"
 
-p = re.compile("[0-9]*")
-for pivID in pivStoryIDs:
-    m = p.match(pivID)
-    if m.group() != None:
-        print(m.group())
+final_pids = []
+pid_f = re.compile("[0-9]*")
+for piv_id in pivStory_ids:
+    m = pid_f.match(piv_id)
+    if m != None:
+        final_pids.append('#' + piv_id)
+
+f = codecs.open(msg_file, "r+", "utf-8")
+all = f.read()
+print(final_pids, file=f)
+print(all, file=f)
+f.close()
+
+
+
 
 
 # if [ "$ManualMessagePivotalID" == "" ] ; then
