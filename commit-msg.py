a89bc473591d327f4b5c6b944ac4e8dc962a9015
Makoto Kameda <makoto.kameda@gmail.com>
2014-03-07 05:52:16 +0100

    [#1234, #111] end
    https://www.pivotaltracker.com/story/show/1234
    https://www.pivotaltracker.com/story/show/111

--- commit-msg.py
+++ commit-msg.py
@@ -10,25 +10,29 @@ msg_file = sys.argv[1]
 branches = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], universal_newlines=True)
 brs = re.search("-(.*?)$", branches.strip("\n"))
 if brs == None:
-    print('fertig')
+    exit()
 pivStory_ids = brs.group(1).split(",")
 
-pivURL = "https://www.pivotaltracker.com/story/show"
+piv_url = "https://www.pivotaltracker.com/story/show/"
 
 final_pids = []
+final_urls = []
 pid_f = re.compile("[0-9]*")
 for piv_id in pivStory_ids:
     m = pid_f.match(piv_id)
     if m != None:
         final_pids.append('#' + piv_id)
+        final_urls.append(piv_url + piv_id)
 
 
 f = codecs.open(msg_file, "r+", "utf-8")
 all = f.read()
-f.seek(0)
 begin_tag = '[' + ', '.join(final_pids) + ']'
+urls = '\n'.join(final_urls)
+f.seek(0)
 print(begin_tag, file=f)
 print(all, file=f)
+print(urls, file=f)
 f.close()
 
 # END
