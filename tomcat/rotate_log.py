8376ad125daf7bcb5d228e854731e88e592410fa
Makoto Kameda <makoto.kameda@gmail.com>
2014-03-21 23:33:29 +0100

    Converted rotate_log from shell to python

--- tomcat/rotate_log.py
+++ tomcat/rotate_log.py
@@ -6,7 +6,8 @@ from stat import *
 import tarfile
 
 now = datetime.date.today()
-age = 6
+archive_age = 7
+delete_age = 30
 tomcat_log_dir = "/usr/local/tomcat/logs/"
 file_list = os.listdir(tomcat_log_dir)
 target_archives = []
@@ -20,28 +21,29 @@ for file in file_list:
         if (now - mtime).days >= 0:
             old_files.append(file)
 
-tar_file = tarfile.open('test.tar.lzma', 'w:xz')
+tar_file = tarfile.open(file + '.tar.lzma', 'w:xz')
 for archive in target_archives:
-    tar_file.addfile(archive)
+    tar_file.add(tomcat_log_dir + archive, arcname=archive)
+    tar_file.close()
+for old_f in old_files:
+    os.remove(tomcat_log_dir + old_f)
 
-            #os.remove(file)
 
 
-
-        #LOG_DIR=/usr/local/tomcat/logs/
-        #
-        #lzmafiles=`find ${LOG_DIR} -mmin +1440 -regex ".*\.\(log\|txt\)$" -printf "%p "`
-        #if [ "$lzmafiles" != "" ] ; then
-        #    lzma ${lzmafiles}
-        #fi
-        #
-        #delfiles=`find ${LOG_DIR} -mmin +43200 -printf "%p "`
-        #if [ "$delfiles" != "" ] ; then
-        #    rm ${delfiles}
-        #fi
-        #
-        #LOG_DIR=/var/log/jenkins/
-        #delfiles=`find ${LOG_DIR} -mmin +43200 -printf "%p "`
-        #if [ "$delfiles" != "" ] ; then
-        #    rm ${delfiles}
-        #fi
+    #LOG_DIR=/usr/local/tomcat/logs/
+    #
+    #lzmafiles=`find ${LOG_DIR} -mmin +1440 -regex ".*\.\(log\|txt\)$" -printf "%p "`
+    #if [ "$lzmafiles" != "" ] ; then
+    #    lzma ${lzmafiles}
+    #fi
+    #
+    #delfiles=`find ${LOG_DIR} -mmin +43200 -printf "%p "`
+    #if [ "$delfiles" != "" ] ; then
+    #    rm ${delfiles}
+    #fi
+    #
+    #LOG_DIR=/var/log/jenkins/
+    #delfiles=`find ${LOG_DIR} -mmin +43200 -printf "%p "`
+    #if [ "$delfiles" != "" ] ; then
+    #    rm ${delfiles}
+    #fi
